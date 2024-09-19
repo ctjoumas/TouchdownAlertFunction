47634fed6f38561e6b66aed70666c71f312c8ff1
ctjoumas <chris.tjoumas@microsoft.com>
2024-09-19 09:08:24 -0400

    Updating timer function for regular season

--- ParsePlaysFunction.cs
+++ ParsePlaysFunction.cs
@@ -52,7 +52,8 @@
         // {month} 9-1 is Sept-Jan
         // {day of week} 0 is Sunday
         [FunctionName("ParseTouchdownsSunday")]
-        public void RunSunday([TimerTrigger("*/10 * 9-23 * 1-2 0")] TimerInfo myTimer, ILogger log, ExecutionContext context)
+        //public void RunSunday([TimerTrigger("*/10 * 9-23 * 1-2 0")] TimerInfo myTimer, ILogger log, ExecutionContext context)
+        public void RunSunday([TimerTrigger("*/10 * 9-23 * 9-12 0")] TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
             log.LogInformation("C# HTTP trigger function processed a request for Sunday games at " + DateTime.Now);
 
@@ -162,7 +163,8 @@
         // {month} 9-1 is Sept-Jan
         // {day of week} 1 is Monday
         [FunctionName("ParseTouchdownsMonday")]
-        public void RunMonday([TimerTrigger("*/10 * 20-23 * 1-2 1")] TimerInfo myTimer, ILogger log, ExecutionContext context)
+        //public void RunMonday([TimerTrigger("*/10 * 20-23 * 1-2 1")] TimerInfo myTimer, ILogger log, ExecutionContext context)
+        public void RunMonday([TimerTrigger("*/10 * 20-23 * 9-12 1")] TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
             log.LogInformation("C# HTTP trigger function processed a request for Monday games at " + DateTime.Now);
 
